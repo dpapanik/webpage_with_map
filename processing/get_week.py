d18be3080372d6d81c2dda9642ed346441de9e61
Eric Helfrich <ericjhelfrich@gmail.com>
2021-02-10 18:43:32 -0500

    heat map and animated lines on map_3

---
+++ processing/get_week.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from geopy import distance
+
+# os.chdir("/mnt/Eric/git/webpage_with_map/processing")
+df = pd.read_csv("../data/202101-capitalbikeshare-tripdata.csv")
+
+# Clean Data
+# Remove extra long distances
+df = df.dropna()
+#df.apply(lambda x: distance.distance((x["start_lat"], x["start_lng"]), x["end_lat"], x["end_lng"]), axis=1)
+
+
+df["started_at"] = pd.to_datetime(df["started_at"])
+
+start_date = pd.to_datetime('1/01/2021 0:00')
+end_date = pd.to_datetime('1/01/2021 23:59')
+jan_1 = df[df["started_at"].between(start_date, end_date)]
+jan_1 = jan_1.sort_values('started_at')
+
+jan_1.to_csv("../data/jan_1_2021.csv")
